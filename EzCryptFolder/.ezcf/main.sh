bbd37908276708afae0669df00fee9b2ef14160a
winterreisender <winterreisender@foxmail.com>
2023-05-06 15:31:28 +0800

    update

--- EzCryptFolder/.ezcf/main.sh
+++ EzCryptFolder/.ezcf/main.sh
@@ -7,14 +7,22 @@ DATA_FILE="${BIN_DIR}/cfdata.7z"
 alias 7za="${BIN_DIR}/7za"
 alias zenity="${BIN_DIR}/zenity"
 
-
 echo BASE_DIR=${BASE_DIR}
 
 password=$(zenity --password)
 
-if [ -f ${DATA_FILE} ]; then
-    7z x -y "-p${password}"  "-o${BASE_DIR}" $DATA_FILE -xr"!.ezcf/"
-    rm $DATA_FILE
-else
+function encrypt_folder() {
     7z a "-p${password}" -r -xr!cfdata -mhe=on -mx=0 $DATA_FILE $BASE_DIR/* -xr"!.ezcf/" -sdel
+}
+
+function decrypt_folder() {
+    7z x -y "-p${password}"  "-o${BASE_DIR}" $DATA_FILE -xr"!.ezcf/" && rm $DATA_FILE
+}
+
+if [ -f ${DATA_FILE} ]; then
+    decrypt_folder
+    zenity --info --text "Decrypted. Press OK to re-encrypt."
 fi
+
+encrypt_folder
+zenity --info --text "Encrypted"
---
+++ EzCryptFolder/.ezcf/start.cmd
@@ -0,0 +1,4 @@
+@echo off
+chcp 65001
+
+busybox bash ./main.sh
