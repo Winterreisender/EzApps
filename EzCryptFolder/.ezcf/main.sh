47995272bcc0c731d920bbc397a620161a4378c0
NOT_DEFINED <NOT_DEFINED@NOT_DEFINED.net>
2023-06-28 19:01:45 +0800

    update EzCryptFolder

--- EzCryptFolder/.ezcf/main.sh
+++ EzCryptFolder/.ezcf/main.sh
@@ -10,18 +10,28 @@ alias zenity="${BIN_DIR}/zenity"
 echo BASE_DIR=${BASE_DIR}
 
 password=$(zenity --password)
+if [ $? -ne 0 ];
+    exit 1
+fi
 
 function encrypt_folder() {
     7z a "-p${password}" -r -xr!cfdata -mhe=on -mx=0 $DATA_FILE $BASE_DIR/* -xr"!.ezcf/" -sdel
+    return $?
 }
 
 function decrypt_folder() {
     7z x -y "-p${password}"  "-o${BASE_DIR}" $DATA_FILE -xr"!.ezcf/" && rm $DATA_FILE
+    return $?
 }
 
 if [ -f ${DATA_FILE} ]; then
     decrypt_folder
-    zenity --info --text "Decrypted. Press OK to re-encrypt."
+    if [ $? -ne 0 ]; then
+        zenity --error --text "Failed to decrypted!"
+        exit 0
+    else
+        zenity --info --text "Decrypted. Press OK to re-encrypt."
+    fi
 fi
 
 encrypt_folder
